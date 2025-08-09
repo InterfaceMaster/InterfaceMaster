dbf80df0cb31d6458c13da376a331596a2ade10c
Muhammet Akdag <muhammet.t.akda@gmail.com>
2025-08-09 14:34:11 +0300

    LICENCE file changed

--- Software/H723_InterfaceMaster/Core/Inc/tasks.h
+++ Software/H723_InterfaceMaster/Core/Inc/tasks.h
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025 InterfaceMaster organization.
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-ShareAlike 4.0 International License. To view a
+ * copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
 /*
  * task.h
  *
@@ -8,8 +15,6 @@
 #ifndef INC_TASKS_H_
 #define INC_TASKS_H_
 
-
 void IM_peripheral_init(void);
-
 
 #endif /* INC_TASKS_H_ */
--- Software/H723_InterfaceMaster/Core/Src/tasks.c
+++ Software/H723_InterfaceMaster/Core/Src/tasks.c
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025 InterfaceMaster organization.
+ * This work is licensed under a Creative Commons
+ * Attribution-NonCommercial-ShareAlike 4.0 International License. To view a
+ * copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
 /*
  * tasks.c
  *
